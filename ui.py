0249856fefdab0441316bd69fc6048d01b7030e3
jayli <jaylinwylie@gmail.com>
2025-04-26 20:46:12 -0700

    Enhanced skein grid sorting, visibility toggling, and event-driven updates

--- ui.py
+++ ui.py
@@ -4,101 +4,27 @@ import json
 import os
 from skein import Skein
 
-wxEVT_COUNT_CHANGED = wx.NewEventType()
-EVT_COUNT_CHANGED = wx.PyEventBinder(wxEVT_COUNT_CHANGED, 1)
 
-
-class CountChangedEvent(wx.PyCommandEvent):
-    def __init__(self, etype, eid, value=0):
-        wx.PyCommandEvent.__init__(self, etype, eid)
-        self.value = value
-
-    def get_value(self):
-        return self.value
-
-
-class SkeinPanel(wx.Panel):
-    def __init__(self, parent, skein, count=0):
-        super().__init__(parent, size=wx.Size(100, 100))
+class ColorDisplayPanel(wx.Panel):
+    def __init__(self, parent, skein):
+        super().__init__(parent, size=wx.Size(100, 50))
         self.skein = skein
-        self.count = count
-        self.SetMinSize(wx.Size(100, 100))
-        self.SetMaxSize(wx.Size(-1, 100))
-
-        average_color = [0, 0, 0]
-        for color in skein.color:
-            average_color[0] += color[0]
-            average_color[1] += color[1]
-            average_color[2] += color[2]
-
-        average_color[0] /= len(skein.color)
-        average_color[1] /= len(skein.color)
-        average_color[2] /= len(skein.color)
-
-        luminance = (0.299 * average_color[0] +
-                     0.587 * average_color[1] +
-                     0.114 * average_color[2]) / 255
-
-        self.text_color = wx.BLACK if luminance > 0.5 else wx.WHITE
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-
-        self.info_label = wx.StaticText(self, label=f"{skein.brand.upper()}\n{skein.sku}\n{skein.name}")
-        self.info_label.SetForegroundColour(self.text_color)
-
-        spinbox_sizer = wx.BoxSizer()
-
-        self.minus_button = wx.Button(self, label="-", size=wx.Size(35, 35))
-        self.minus_button.Bind(wx.EVT_BUTTON, self._decrease_value)
-
-        self.value_text = wx.TextCtrl(self, value=str(count), size=wx.Size(70, 35), style=wx.TE_CENTER)
-
-        self.plus_button = wx.Button(self, label="+", size=wx.Size(35, 35))
-        self.plus_button.Bind(wx.EVT_BUTTON, self._increase_value)
-
-        spinbox_sizer.Add(self.minus_button, 0, wx.ALIGN_CENTER)
-        spinbox_sizer.Add(self.value_text, 0, wx.ALIGN_CENTER)
-        spinbox_sizer.Add(self.plus_button, 0, wx.ALIGN_CENTER)
-
-        sizer.Add(self.info_label, 1, wx.ALIGN_CENTER | wx.ALL, 5)
-        sizer.Add(spinbox_sizer, 0, wx.ALIGN_CENTER | wx.BOTTOM, 5)
-
-        self.SetSizer(sizer)
-
+        self.SetMinSize(wx.Size(100, 50))
         self.Bind(wx.EVT_PAINT, self.on_paint)
-
-    def _decrease_value(self, event):
-        current = int(self.value_text.GetValue())
-        if current > 0:
-            new_value = current - 1
-            self.value_text.SetValue(str(new_value))
-            self.count = new_value
-            evt = CountChangedEvent(wxEVT_COUNT_CHANGED, self.GetId(), new_value)
-            wx.PostEvent(self, evt)
-
-    def _increase_value(self, event):
-        current = int(self.value_text.GetValue())
-        if current < 999:
-            new_value = current + 1
-            self.value_text.SetValue(str(new_value))
-            self.count = new_value
-            evt = CountChangedEvent(wxEVT_COUNT_CHANGED, self.GetId(), new_value)
-            wx.PostEvent(self, evt)
 
     def on_paint(self, event):
         dc = wx.PaintDC(self)
-        gc = wx.GraphicsContext.Create(dc)
 
         # Get the size of the panel
         width, height = self.GetSize()
 
         # Draw background
-        gc.SetBrush(wx.Brush(wx.Colour(64, 64, 64)))
-        gc.DrawRectangle(0, 0, width, height)
+        # dc.SetBrush(wx.Brush(wx.Colour(64, 64, 64)))
+        dc.SetPen(wx.TRANSPARENT_PEN)
+        dc.DrawRectangle(0, 0, width, height)
 
         # Draw border
-        gc.SetPen(wx.Pen(wx.BLACK, 1))
-        gc.DrawRectangle(0, 0, width, height)
+        dc.DrawRectangle(0, 0, width, height)
 
         # Draw color bands
         colors = self.skein.color
@@ -109,21 +35,92 @@ class SkeinPanel(wx.Panel):
         if num_colors == 1:
             # Single color - fill the whole area
             r, g, b = colors[0]
-            gc.SetBrush(wx.Brush(wx.Colour(r, g, b)))
-            gc.DrawRectangle(0, 0, width, height)
+            dc.SetBrush(wx.Brush(wx.Colour(r, g, b)))
+            dc.DrawRectangle(0, 0, width, height)
         else:
             # Multiple colors - draw horizontal bands
             band_width = width / num_colors
             for i, color in enumerate(colors):
                 r, g, b = color
-                gc.SetBrush(wx.Brush(wx.Colour(r, g, b)))
-                gc.DrawRectangle(i * band_width, 0, band_width, height)
+                dc.SetBrush(wx.Brush(wx.Colour(r, g, b)))
+                dc.DrawRectangle(int(i * band_width), 0, int(band_width), height)
+
+
+class SkeinPanel(wx.Panel):
+    def __init__(self, parent, skein, count=0, callback=None):
+        super().__init__(parent, size=wx.Size(100, 100))
+        self.skein = skein
+        self.count = count
+        self.brand = skein.brand
+        self.sku = skein.sku
+        self.callback = callback
+        self.SetMinSize(wx.Size(100, 100))
+        self.SetMaxSize(wx.Size(-1, 100))
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Add text in a dedicated area above the color panel
+        text = f"{self.skein.brand.upper()}\n{self.skein.sku}\n{self.skein.name}"
+        self.text_label = wx.StaticText(self, label=text, style=wx.ALIGN_CENTER)
+        self.text_label.SetForegroundColour(wx.BLACK)
+        font = wx.Font(9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.text_label.SetFont(font)
+
+        sizer.Add(self.text_label, 0, wx.ALIGN_CENTER | wx.TOP, 5)
+
+        # Add color display panel
+        self.color_panel = ColorDisplayPanel(self, skein)
+        sizer.Add(self.color_panel, 1, wx.EXPAND | wx.ALL, 5)
+
+        # Add spinbox at the bottom
+        spinbox_sizer = wx.BoxSizer()
+
+        self.minus_button = wx.Button(self, label="-", size=wx.Size(35, 20))
+        self.minus_button.Bind(wx.EVT_BUTTON, self._decrease_value)
+
+        self.value_text = wx.TextCtrl(self, value=str(count), size=wx.Size(70, 20), style=wx.TE_CENTER)
+        self.value_text.Bind(wx.EVT_TEXT, self._set_value)
+
+        self.plus_button = wx.Button(self, label="+", size=wx.Size(35, 20))
+        self.plus_button.Bind(wx.EVT_BUTTON, self._increase_value)
+
+        spinbox_sizer.Add(self.minus_button, 0, wx.ALIGN_CENTER)
+        spinbox_sizer.Add(self.value_text, 0, wx.ALIGN_CENTER)
+        spinbox_sizer.Add(self.plus_button, 0, wx.ALIGN_CENTER)
+
+        sizer.Add(spinbox_sizer, 0, wx.ALIGN_CENTER | wx.BOTTOM, 5)
+
+        self.SetSizer(sizer)
+
+    def _decrease_value(self, event):
+        current = int(self.value_text.GetValue())
+        if current > 0:
+            new_value = current - 1
+            self.value_text.SetValue(str(new_value))
+            self.count = new_value
+            if self.callback:
+                self.callback(self.brand, self.sku, new_value)
+
+    def _increase_value(self, event):
+        current = int(self.value_text.GetValue())
+        if current < 999:
+            new_value = current + 1
+            self.value_text.SetValue(str(new_value))
+            self.count = new_value
+            if self.callback:
+                self.callback(self.brand, self.sku, new_value)
+
+    def _set_value(self, event):
+        new_value = int(self.value_text.GetValue())
+        self.count = new_value
+        if self.callback:
+            self.callback(self.brand, self.sku, new_value)
 
 
 class ColorPanel(wx.Panel):
-    def __init__(self, parent, color=None):
+    def __init__(self, parent, color: wx.Colour):
         super().__init__(parent, size=wx.Size(50, 50))
-        self.color = color or [255, 255, 255]
+        self.color = list(wx.Colour(color).Get())
         self.SetMinSize(wx.Size(50, 50))
         self.SetMaxSize(wx.Size(50, 50))
 
@@ -133,34 +130,27 @@ class ColorPanel(wx.Panel):
 
     def OnPaint(self, event):
         dc = wx.PaintDC(self)
-        gc = wx.GraphicsContext.Create(dc)
-
         # Get the size of the panel
         width, height = self.GetSize()
 
         # Draw border
-        gc.SetPen(wx.Pen(wx.BLACK, 1))
-        gc.DrawRectangle(0, 0, width, height)
+        dc.SetPen(wx.Pen(wx.BLACK, 0))
+        dc.SetBrush(wx.TRANSPARENT_BRUSH)
+        dc.DrawRectangle(0, 0, width, height)
 
         # Fill with color
-        r, g, b = self.color
-        gc.SetBrush(wx.Brush(wx.Colour(r, g, b)))
-        gc.DrawRectangle(1, 1, width-2, height-2)
+        dc.SetBrush(wx.Brush(wx.Colour(*self.color)))
+        dc.SetPen(wx.TRANSPARENT_PEN)
+        dc.DrawRectangle(1, 1, width-2, height-2)
 
     def on_click(self, event):
         color_data = wx.ColourData()
-        color_data.SetColour(wx.Colour(self.color[0], self.color[1], self.color[2]))
-
+        color_data.SetColour(wx.Colour(*self.color))
         dlg = wx.ColourDialog(self, color_data)
-        if dlg.ShowModal() == wx.ID_OK:
-            wx_color = dlg.GetColourData().GetColour()
-            self.color = [wx_color.Red(), wx_color.Green(), wx_color.Blue()]
-            self.Refresh()
 
-            # Notify parent of color change
-            evt = wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED, self.GetId())
-            evt.SetEventObject(self)
-            wx.PostEvent(self.GetParent(), evt)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.color = list(dlg.GetColourData().GetColour().Get())
+            self.Refresh()
 
         dlg.Destroy()
 
@@ -168,8 +158,8 @@ class ColorPanel(wx.Panel):
 class AddSkeinDialog(wx.Dialog):
     def __init__(self, parent, catalog):
         super().__init__(parent, title="Add New Skein")
+        self.color_panels: list[ColorPanel] = []
         self.catalog = catalog
-        self.colors = [[255, 255, 255]]  # Default to white
 
         # Create the main sizer
         main_sizer = wx.BoxSizer(wx.VERTICAL)
@@ -209,7 +199,7 @@ class AddSkeinDialog(wx.Dialog):
         self.colors_panel.SetSizer(self.colors_sizer)
 
         # Add initial color panel
-        self.add_color_panel(self.colors[0])
+        self.add_color(wx.WHITE)
 
         colors_sizer.Add(self.colors_panel, 0, wx.EXPAND | wx.ALL, 5)
 
@@ -239,43 +229,31 @@ class AddSkeinDialog(wx.Dialog):
         main_sizer.SetMinSize(wx.Size(400, 300))
         main_sizer.Fit(self)
 
-    def add_color_panel(self, color):
-        color_panel = ColorPanel(self.colors_panel, color)
-        color_panel.Bind(wx.EVT_BUTTON, self.update_color)
+    def add_color(self, event):
+        color_panel = ColorPanel(self.colors_panel, wx.WHITE)
+        self.color_panels.append(color_panel)
         self.colors_sizer.Add(color_panel, 0, wx.RIGHT, 5)
         self.colors_panel.Layout()
 
-    def add_color(self, event):
-        self.colors.append([255, 255, 255])
-        self.add_color_panel(self.colors[-1])
-
     def remove_color(self, event):
-        if len(self.colors) > 1:
-            self.colors.pop()
+        if len(self.color_panels) > 1:
             # Remove the last color panel
-            self.colors_sizer.GetItem(self.colors_sizer.GetItemCount() - 1).GetWindow().Destroy()
+            self.color_panels.pop().Destroy()
             self.colors_panel.Layout()
 
-    def update_color(self, event):
-        # Find which color panel was changed
-        panel = event.GetEventObject()
-        index = 0
-        for i in range(self.colors_sizer.GetItemCount()):
-            if self.colors_sizer.GetItem(i).GetWindow() == panel:
-                index = i
-                break
-        self.colors[index] = panel.color
+    def save_skein(self):
+        colors = []
+        for color_panel in self.color_panels:
+            selected_color: list = color_panel.color
+            colors.append([selected_color[0], selected_color[1], selected_color[2]])
 
-    def get_skein_data(self):
-        return {
+        data = {
             "brand": self.brand_input.GetValue(),
             "sku": self.sku_input.GetValue(),
             "name": self.name_input.GetValue(),
-            "colors": self.colors
+            "color": colors
         }
 
-    def save_skein(self):
-        data = self.get_skein_data()
         brand = data["brand"]
         sku = data["sku"]
 
@@ -299,7 +277,7 @@ class AddSkeinDialog(wx.Dialog):
         # Add or update skein data
         brand_data[sku] = {
             "name": data["name"],
-            "color": data["colors"]
+            "color": data["color"]
         }
 
         # Save brand file
@@ -316,7 +294,7 @@ class AddSkeinDialog(wx.Dialog):
 
         skein = Skein(brand, sku)
         skein.name = data["name"]
-        skein.color = data["colors"]
+        skein.color = data["color"]
         self.catalog.skeins[brand][sku] = skein
 
         return True
@@ -325,6 +303,7 @@ class AddSkeinDialog(wx.Dialog):
 class Window(wx.Frame):
     def __init__(self, library, catalog, parent=None):
         super().__init__(parent, title="Skein Care", size=wx.Size(800, 600))
+        self.visible_skeins = None
         self.library = library
         self.catalog = catalog
         self.show_all = True  # Default to showing all skeins
@@ -339,10 +318,9 @@ class Window(wx.Frame):
         menubar = wx.MenuBar()
         file_menu = wx.Menu()
 
-
         # Add "Add New Skein" menu item
         add_skein_item = file_menu.Append(wx.ID_ANY, "Add New Skein")
-        self.Bind(wx.EVT_MENU, self.show_add_skein_dialog, add_skein_item)
+        self.Bind(wx.EVT_MENU, self.add_skein, add_skein_item)
 
         # Add separator
         file_menu.AppendSeparator()
@@ -387,18 +365,46 @@ class Window(wx.Frame):
 
         self.panel.SetSizer(main_sizer)
 
-        # Populate the grid with skeins
+        self.collect_visible_skeins()
         self.populate_grid()
 
-        # Set a minimum size
-        self.SetMinSize(self.GetSize())
+        # Hack to refresh layout
+        self.SetSize(wx.Size(self.GetSize()[0], self.GetSize()[1] + 1))
 
     def populate_grid(self):
         # Clear existing widgets
         self.grid_sizer.Clear(True)
 
+        if self.sort_method == 0:  # Sort by brand
+            self.visible_skeins.sort(key=lambda x: x[0].lower())  # Sort by brand (case-insensitive)
+        elif self.sort_method == 1:  # Sort by SKU
+            self.visible_skeins.sort(key=lambda x: int(x[1]) if x[1].isdecimal() else -1)  # Sort by SKU (case-insensitive)
+        elif self.sort_method == 2:  # Sort by name
+            self.visible_skeins.sort(key=lambda x: x[2].name.lower())  # Sort by name (case-insensitive)
+        elif self.sort_method == 3:  # Sort by count
+            self.visible_skeins.sort(key=lambda x: x[3], reverse=True)  # Sort by count (descending)
+
+        # Freeze the window to prevent flickering
+        self.scroll.Freeze()
+
+        # Add sorted skeins to the grid
+        for brand, sku, skein, count in self.visible_skeins:
+            # Create skein panel with callback
+            skein_panel = SkeinPanel(self.scroll, skein, count, callback=self.update_skein_count)
+
+            # Add to grid
+            self.grid_sizer.Add(skein_panel, 0, wx.EXPAND)
+
+        # Update layout
+        self.grid_sizer.Layout()
+        self.scroll.FitInside()
+
+        # Thaw the window to allow updates
+        self.scroll.Thaw()
+
+    def collect_visible_skeins(self):
         # Collect all visible skeins
-        visible_skeins = []
+        self.visible_skeins = []
         for brand, brand_skeins in self.catalog.skeins.items():
             for sku, skein in brand_skeins.items():
                 # Check if we should show this skein
@@ -411,36 +417,13 @@ class Window(wx.Frame):
 
                 # Check if skein matches search criteria
                 if self.search_text and not (
-                    self.search_text.lower() in sku.lower() or
-                    self.search_text.lower() in skein.name.lower()
+                        self.search_text.lower() in sku.lower() or
+                        self.search_text.lower() in skein.name.lower()
                 ):
                     continue  # Skip if doesn't match search
 
                 # Add to visible skeins
-                visible_skeins.append((brand, sku, skein, count))
-
-        # Sort the skeins based on the selected sort method
-        if self.sort_method == 0:  # Sort by brand
-            visible_skeins.sort(key=lambda x: x[0].lower())  # Sort by brand (case-insensitive)
-        elif self.sort_method == 1:  # Sort by SKU
-            visible_skeins.sort(key=lambda x: int(x[1]) if x[1].isdecimal() else -1)  # Sort by SKU (case-insensitive)
-        elif self.sort_method == 2:  # Sort by name
-            visible_skeins.sort(key=lambda x: x[2].name.lower())  # Sort by name (case-insensitive)
-        elif self.sort_method == 3:  # Sort by count
-            visible_skeins.sort(key=lambda x: x[3], reverse=True)  # Sort by count (descending)
-
-        # Add sorted skeins to the grid
-        for brand, sku, skein, count in visible_skeins:
-            # Create skein panel
-            skein_panel = SkeinPanel(self.scroll, skein, count)
-            skein_panel.Bind(EVT_COUNT_CHANGED, lambda evt, b=brand, s=sku: self.update_skein_count(b, s, evt.get_value()))
-
-            # Add to grid
-            self.grid_sizer.Add(skein_panel, 0, wx.EXPAND)
-
-        # Update layout
-        self.grid_sizer.Layout()
-        self.scroll.FitInside()
+                self.visible_skeins.append((brand, sku, skein, count))
 
     def sort_skeins(self, event):
         id = event.GetId()
@@ -459,25 +442,25 @@ class Window(wx.Frame):
 
         self.populate_grid()
 
-
-
     def toggle_skeins_visibility(self, event):
         self.show_all = event.IsChecked()
         if self.show_all:
             self.toggle_item.SetItemLabel("Show All Skeins")
         else:
             self.toggle_item.SetItemLabel("Show Library Only")
+        self.collect_visible_skeins()
         self.populate_grid()
 
     def search(self, event):
         self.search_text = event.GetString()
         self.populate_grid()
 
-    def show_add_skein_dialog(self, event):
+    def add_skein(self, event):
         dialog = AddSkeinDialog(self, self.catalog)
         if dialog.ShowModal() == wx.ID_OK:
             if dialog.save_skein():
                 wx.MessageBox("Skein added successfully.", "Success", wx.OK | wx.ICON_INFORMATION)
+                self.collect_visible_skeins()
                 self.populate_grid()
         dialog.Destroy()
 
