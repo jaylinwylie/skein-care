8dad8e21505352c85514409f2fe03ea0ef002ef9
jaylinwylie <jaylinwylie@gmail.com>
2025-08-05 15:52:11 -0700

    Remove Windows-specific files; Add macOS build spec; Update update version to v1.1.0

--- mac_gui.spec
+++ mac_gui.spec
@@ -18,18 +18,31 @@ pyz = PYZ(a.pure)
 exe = EXE(
     pyz,
     a.scripts,
-    a.binaries,
-    a.datas,
+    [],
+    exclude_binaries=True,
     name='Skein Care',
-    icon='thread.png',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
     upx=True,
     console=False,
     disable_windowed_traceback=False,
-    argv_emulation=False,
+    argv_emulation=True,
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
+)
+
+app = BUNDLE(
+    exe,
+    a.binaries,
+    a.datas,
+    name='Skein Care.app',
+    icon='thread.png',
+    bundle_identifier=None,
+    info_plist={
+        'NSHighResolutionCapable': 'True',
+        'NSPrincipalClass': 'NSApplication',
+        'CFBundleShortVersionString': '1.0.0',
+    },
 )
--- updater.py
+++ updater.py
@@ -1,98 +1,19 @@
-# query format - "v#.#.#
-# data format (int, int, int)
-# Example data
-# {
-#   "url": "https://api.github.com/repos/jaylinwylie/sbv2txt/releases/233763552",
-#   "assets_url": "https://api.github.com/repos/jaylinwylie/sbv2txt/releases/233763552/assets",
-#   "upload_url": "https://uploads.github.com/repos/jaylinwylie/sbv2txt/releases/233763552/assets{?name,label}",
-#   "html_url": "https://github.com/jaylinwylie/sbv2txt/releases/tag/v1.0.1",
-#   "id": 233763552,
-#   "author": {
-#     "login": "jaylinwylie",
-#     "id": 79897627,
-#     "node_id": "MDQ6VXNlcjc5ODk3NjI3",
-#     "avatar_url": "https://avatars.githubusercontent.com/u/79897627?v=4",
-#     "gravatar_id": "",
-#     "url": "https://api.github.com/users/jaylinwylie",
-#     "html_url": "https://github.com/jaylinwylie",
-#     "followers_url": "https://api.github.com/users/jaylinwylie/followers",
-#     "following_url": "https://api.github.com/users/jaylinwylie/following{/other_user}",
-#     "gists_url": "https://api.github.com/users/jaylinwylie/gists{/gist_id}",
-#     "starred_url": "https://api.github.com/users/jaylinwylie/starred{/owner}{/repo}",
-#     "subscriptions_url": "https://api.github.com/users/jaylinwylie/subscriptions",
-#     "organizations_url": "https://api.github.com/users/jaylinwylie/orgs",
-#     "repos_url": "https://api.github.com/users/jaylinwylie/repos",
-#     "events_url": "https://api.github.com/users/jaylinwylie/events{/privacy}",
-#     "received_events_url": "https://api.github.com/users/jaylinwylie/received_events",
-#     "type": "User",
-#     "user_view_type": "public",
-#     "site_admin": false
-#   },
-#   "node_id": "RE_kwDOPPkFys4N7vLg",
-#   "tag_name": "v1.0.1",
-#   "target_commitish": "master",
-#   "name": "sbv2txt-windows-1.0.1",
-#   "draft": false,
-#   "immutable": false,
-#   "prerelease": false,
-#   "created_at": "2025-07-20T17:00:51Z",
-#   "published_at": "2025-07-20T17:15:25Z",
-#   "assets": [
-#     {
-#       "url": "https://api.github.com/repos/jaylinwylie/sbv2txt/releases/assets/274660631",
-#       "id": 274660631,
-#       "node_id": "RA_kwDOPPkFys4QXv0X",
-#       "name": "sbv2txt-windows-1.0.1.zip",
-#       "label": null,
-#       "uploader": {
-#         "login": "jaylinwylie",
-#         "id": 79897627,
-#         "node_id": "MDQ6VXNlcjc5ODk3NjI3",
-#         "avatar_url": "https://avatars.githubusercontent.com/u/79897627?v=4",
-#         "gravatar_id": "",
-#         "url": "https://api.github.com/users/jaylinwylie",
-#         "html_url": "https://github.com/jaylinwylie",
-#         "followers_url": "https://api.github.com/users/jaylinwylie/followers",
-#         "following_url": "https://api.github.com/users/jaylinwylie/following{/other_user}",
-#         "gists_url": "https://api.github.com/users/jaylinwylie/gists{/gist_id}",
-#         "starred_url": "https://api.github.com/users/jaylinwylie/starred{/owner}{/repo}",
-#         "subscriptions_url": "https://api.github.com/users/jaylinwylie/subscriptions",
-#         "organizations_url": "https://api.github.com/users/jaylinwylie/orgs",
-#         "repos_url": "https://api.github.com/users/jaylinwylie/repos",
-#         "events_url": "https://api.github.com/users/jaylinwylie/events{/privacy}",
-#         "received_events_url": "https://api.github.com/users/jaylinwylie/received_events",
-#         "type": "User",
-#         "user_view_type": "public",
-#         "site_admin": false
-#       },
-#       "content_type": "application/x-zip-compressed",
-#       "state": "uploaded",
-#       "size": 7062221,
-#       "digest": "sha256:361991b740057fcba94088e3e8a941d85dd5185029378629e2d3d3473765bbc3",
-#       "download_count": 0,
-#       "created_at": "2025-07-20T17:15:17Z",
-#       "updated_at": "2025-07-20T17:15:19Z",
-#       "browser_download_url": "https://github.com/jaylinwylie/sbv2txt/releases/download/v1.0.1/sbv2txt-windows-1.0.1.zip"
-#     }
-#   ],
-#   "tarball_url": "https://api.github.com/repos/jaylinwylie/sbv2txt/tarball/v1.0.1",
-#   "zipball_url": "https://api.github.com/repos/jaylinwylie/sbv2txt/zipball/v1.0.1",
-#   "body": "**Full Changelog**: https://github.com/jaylinwylie/sbv2txt/compare/v1.0.0...v1.0.1"
-# }
-
 import requests
-VERSION = "v0.0.0"
+VERSION = "v1.1.0"
 
 USER = "jaylinwylie"
 REPO = "skein-care"
 GITHUB_RELEASES_URL = f"https://github.com/{USER}/{REPO}/releases"
 KO_FI_URL = "https://ko-fi.com/s/011d38ab3b"
 
+
 def to_version(tag: str) -> tuple[int, ...]:
+    """Convert a tag string to a tuple of integers. Example: "v1.2.3" -> (1, 2, 3)"""
     return tuple(map(int, tag[1:].split(".")))
 
 
 def is_newer_version(current: tuple, latest: tuple):
+    """Check if the current version is newer than the latest version"""
     for i in range(3):
         if current[i] < latest[i]:
             return True
@@ -102,6 +23,7 @@ def is_newer_version(current: tuple, latest: tuple):
 
 
 def query_latest(user, repo) -> tuple[bool, str | dict]:
+    """Query the latest version of the app from GitHub"""
     try:
         url = f"https://api.github.com/repos/{user}/{repo}/releases/latest"
         response = requests.get(url, timeout=10)  # Add timeout to prevent hanging
